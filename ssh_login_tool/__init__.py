c535f76b51d34b4a04412e9670fac22bae6dedc3
denglinfeng@leinao.ai <testlin123>
2020-07-14 16:37:45 +0800

    1.处理不需要密码就能登陆的SSH 2.处理第一次ssh的时候，需要输入一个yes

--- ssh_login_tool/__init__.py
+++ ssh_login_tool/__init__.py
@@ -10,23 +10,34 @@ import pexpect
 envs = {'TEST1': ['user', 'port', '10.1.1.1', 'p1'], 'TEST2': ['user', 'port', '10.2.2.2', 'p2'],
         'TEST3': ['user', 'port', '10.2.2.3', 'p4']}
 
-
+'''
+expect方法的第二个参数pattern是一个多值参数，可以传入多个判断条件
+它的返回index==0是指一个都没匹配到
+index==1表示匹配到了第一个，所以这里当index==2,表示，不需要密码就能登陆
+'''
 def ssh_login(env):
+    ssh_newkey = "Are you sure you want to continue connecting"
+    connected_key = "]#"
     username, port, ip, passw = select_envs(env)
     port = 22 if port is None else port
-    ssh_cmd = pexpect.spawn('ssh %s@%s -p %s -y' % (username, ip, port), timeout=30)
+    ssh_cmd = pexpect.spawn('ssh %s@%s -p %s -y' % (username, ip, port), timeout=1)
     fout = file('mylog.txt', 'w')
     ssh_cmd.logfile = fout
-    password_index = ssh_cmd.expect('password')
-    if password_index == 0:
-        ssh_cmd.sendline(passw)
-        index = ssh_cmd.expect('$')
-        if index != 0:
-            exit(b'password is not correct.')
-        print(ssh_cmd.before)
-        ssh_cmd.interact()
-    else:
-        print('the index is:%d,cannot find something.' % password_index)
+    ret = ssh_cmd.expect([pexpect.TIMEOUT, ssh_newkey, connected_key, '[P|p]assword:'])
+    print()
+    if ret == 0:
+        print('[-] Error Connecting')
+        return
+    if ret == 1:
+        ssh_cmd.sendline('yes')
+        ret = ssh_cmd.expect([pexpect.TIMEOUT, ssh_newkey, connected_key, '[P|p]assword:'])
+        if ret == 0:
+            print('[-] Error Connecting')
+            return
+    ssh_cmd.sendline(passw)
+    ssh_cmd.expect(PROMPT)
+    print(ssh_cmd.before)
+    ssh_cmd.interact()
     ssh_cmd.close()
 
 
